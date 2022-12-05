2823a122d36638c2c8d682acb19b55cdd50acc39
Pham Thanh Nam <zvw180ptn1411@gmail.com>
2022-12-05 11:29:02 +0700

    ddd

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -5,6 +5,7 @@ const router = Router();
 router.get("/", (_req: Request, res: Response) => {
   res.send("Server");
 });
+
 router.get("/dev", (_req: Request, res: Response) => {
   res.send("Server dev");
 });
