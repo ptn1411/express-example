c335ee7c5b0c0f212b4a8481b3d855431a5b6e8a
Pham Thanh Nam <zvw180ptn1411@gmail.com>
2022-12-07 10:08:30 +0700

    router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -9,16 +9,5 @@ router.get("/", (_req: Request, res: Response) => {
 router.get("/dev", (_req: Request, res: Response) => {
   res.send("Server dev");
 });
-router.get("/3005", (_req: Request, res: Response) => {
-  res.send("Server 3005");
-});
-router.get("/3006", (_req: Request, res: Response) => {
-  res.send("Server 3006");
-});
-router.get("/3007", (_req: Request, res: Response) => {
-  res.send("Server 3005");
-});
-router.get("/3008", (_req: Request, res: Response) => {
-  res.send("Server 3006");
-});
+
 export default router;
